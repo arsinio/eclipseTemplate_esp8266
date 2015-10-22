02a586ac9955530be4f1fd349e9bb021e372a18b
Christopher Armenio <christopher.armenio@opencxa.org>
2015-10-22 01:39:07 -0400

    compiling, flashing, and running (albeit with some undefined symbols)...still need to extract core libraries/includes

---
+++ project/include/cxa_config.h
@@ -0,0 +1,28 @@
+/*
+ * cxa_config.h
+ *
+ * Created: 5/25/2015 8:55:08 PM
+ *  Author: Christopher Armenio
+ */ 
+
+#ifndef CXA_CONFIG_H_
+#define CXA_CONFIG_H_
+
+#define CXA_LOGGER_TIME_ENABLE
+#define CXA_LOGGER_BUFFERLEN_BYTES					32
+
+#define CXA_ASSERT_EXIT_FUNC						while(1);
+#define CXA_ASSERT_GPIO_FLASH_ENABLE
+#define CXA_ASSERT_LINE_NUM_ENABLE
+#define CXA_ASSERT_MSG_ENABLE
+
+#define CXA_STATE_MACHINE_ENABLE_LOGGING
+
+#define CXA_FILE_DISABLE
+#define CXA_LINE_ENDING								"\r\n"
+
+#define CXA_RPC_NODE_MAX_NAME_LEN_BYTES				36
+#define CXA_RPC_MSGFACTORY_POOL_NUM_MSGS			4
+
+
+#endif /* CXA_CONFIG_H_ */
--- project/src/main.c
+++ project/src/main.c
@@ -1,12 +1,20 @@
 
 
+#include <cxa_arduino_gpio.h>
+#include <cxa_delay.h>
+
+
+static cxa_arduino_gpio_t led_red;
+
+
 void setup()
 {
-
+	cxa_arduino_gpio_init_output(&led_red, 0, CXA_GPIO_POLARITY_NONINVERTED, 0);
 }
 
 
 void loop()
 {
-
+	cxa_gpio_toggle(&led_red.super);
+	cxa_delay_ms(250);
 }
