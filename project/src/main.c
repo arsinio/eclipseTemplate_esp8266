218878696d9b4dbaf0ac56efd604787186b089ee
Christopher Armenio <christopher.armenio@opencxa.org>
2015-11-16 21:19:29 -0500

    updated LED function to make a little clearer of a demo

--- project/src/main.c
+++ project/src/main.c
@@ -21,11 +21,12 @@
 #include <cxa_esp8266_usart.h>
 #include <cxa_esp8266_timeBase.h>
 #include <cxa_esp8266_wifiManager.h>
-#include <cxa_esp8266_network_clientFactory.h>
+#include <cxa_esp8266_network_factory.h>
 
 #include <cxa_timeDiff.h>
 
 #include <cxa_mqtt_client_network.h>
+#include <cxa_esp8266_network_tcpServer.h>
 
 
 // ******** local macro definitions ********
@@ -33,17 +34,13 @@
 #define LED_OFFPERIOD_ASSOCIATING_MS		500
 #define LED_ONPERIOD_ASSOCIATED_MS			1000
 #define LED_OFFPERIOD_ASSOCIATED_MS			1000
-#define LED_ONPERIOD_CONFIG_NOCONN			100
-#define LED_OFFPERIOD_CONFIG_NOCONN			900
-#define LED_ONPERIOD_CONFIG_CONN			900
-#define LED_OFFPERIOD_CONFIG_CONN			100
+#define LED_ONPERIOD_CONFIG					100
+#define LED_OFFPERIOD_CONFIG				900
 
 
 // ******** local function prototoypes ********
 static void updateLed(void);
 static void wifiManCb_configMode_enter(void* userVarIn);
-static void wifiManCb_numConnChange(uint8_t numConnStations, void* userVar);
-static void wifiManCb_associating(const char *const ssidIn, void* userVarIn);
 static void wifiManCb_associated(const char *const ssidIn, void* userVarIn);
 
 static void mqttCb_onConnect(cxa_mqtt_client_t *const clientIn, void* userVarIn);
@@ -53,7 +50,7 @@ static void mqttCb_onPublish_testTopic(cxa_mqtt_client_t *const clientIn, char* 
 // ******** local variable declarations ********
 static cxa_esp8266_gpio_t led_red;
 
-static cxa_esp8266_usart_t usart;
+static cxa_esp8266_usart_t usart_log;
 static cxa_ioStream_t* ios_usart;
 static cxa_timeBase_t tb_generalPurpose;
 
@@ -61,7 +58,7 @@ static uint32_t led_onPeriod_ms = 0;
 static uint32_t led_offPeriod_ms = 0;
 static cxa_timeDiff_t td_blink;
 
-static cxa_mqtt_client_network_t mqttC;
+static cxa_mqtt_client_network_t mqttClient;
 
 
 // ******** global function implementations ********
@@ -71,39 +68,40 @@ void setup(void)
 	cxa_esp8266_gpio_init_output(&led_red, 0, CXA_GPIO_POLARITY_INVERTED, 0);
 	cxa_assert_setAssertGpio(&led_red.super);
 
-
-	// setup our debug usart (and delay so things can settle)
-	cxa_esp8266_usart_init_noHH(&usart, CXA_ESP8266_USART_0, 115200);
-	ios_usart = cxa_usart_getIoStream(&usart.super);
+	// setup our logging usart
+	cxa_esp8266_usart_init_noHH(&usart_log, CXA_ESP8266_USART_0, 115200);
+	ios_usart = cxa_usart_getIoStream(&usart_log.super);
 	cxa_assert_setIoStream(ios_usart);
 
 	// setup our general-purpose timebase
 	cxa_esp8266_timeBase_init(&tb_generalPurpose);
+	cxa_timeDiff_init(&td_blink, &tb_generalPurpose, true);
 
 	// setup our logging system
 	cxa_logger_setGlobalTimeBase(&tb_generalPurpose);
 	cxa_logger_setGlobalIoStream(ios_usart);
 
-	// setup our application specific code here
-	cxa_timeDiff_init(&td_blink, &tb_generalPurpose, true);
-
+	// setup our wifi
 	cxa_esp8266_wifiManager_init(NULL, &tb_generalPurpose);
-	cxa_esp8266_wifiManager_addListener(wifiManCb_configMode_enter, wifiManCb_numConnChange, NULL, wifiManCb_associating, wifiManCb_associated, NULL, NULL, NULL);
+	cxa_esp8266_wifiManager_addListener(wifiManCb_configMode_enter, NULL, NULL, NULL, wifiManCb_associated, NULL, NULL, NULL);
 	cxa_esp8266_wifiManager_addStoredNetwork("yourSsid", "yourPassphrase");
 	cxa_esp8266_wifiManager_start();
 
-	cxa_esp8266_network_clientFactory_init(&tb_generalPurpose);
-	cxa_mqtt_client_network_init(&mqttC, &tb_generalPurpose, "cid");
-	cxa_mqtt_client_addListener(&mqttC.super, mqttCb_onConnect, NULL, NULL);
-	cxa_mqtt_client_subscribe(&mqttC.super, "testTopic", CXA_MQTT_QOS_ATMOST_ONCE, mqttCb_onPublish_testTopic, NULL);
+	// setup our network factory
+	cxa_esp8266_network_factory_init(&tb_generalPurpose);
+
+	// setup our mqtt client
+	cxa_mqtt_client_network_init(&mqttClient, &tb_generalPurpose, "cid");
+	cxa_mqtt_client_addListener(&mqttClient.super, mqttCb_onConnect, NULL, NULL);
+	cxa_mqtt_client_subscribe(&mqttClient.super, "testTopic", CXA_MQTT_QOS_ATMOST_ONCE, mqttCb_onPublish_testTopic, NULL);
 }
 
 
 void loop(void)
 {
 	cxa_esp8266_wifiManager_update();
-	cxa_esp8266_network_clientFactory_update();
-	cxa_mqtt_client_update(&mqttC.super);
+	cxa_esp8266_network_factory_update();
+	cxa_mqtt_client_update(&mqttClient.super);
 
 	updateLed();
 }
@@ -112,6 +110,9 @@ void loop(void)
 // ******** local function implementations ********
 static void updateLed(void)
 {
+	// don't touch the LEDs if we're connected
+	if( cxa_mqtt_client_isConnected(&mqttClient.super) ) return;
+
 	if( led_offPeriod_ms == 0 ) cxa_gpio_setValue(&led_red.super, 1);
 	else if( led_onPeriod_ms == 0 ) cxa_gpio_setValue(&led_red.super, 0);
 	else if( cxa_gpio_getValue(&led_red.super) && cxa_timeDiff_isElapsed_recurring_ms(&td_blink, led_onPeriod_ms) ) cxa_gpio_setValue(&led_red.super, 0);
@@ -121,30 +122,8 @@ static void updateLed(void)
 
 static void wifiManCb_configMode_enter(void* userVarIn)
 {
-	led_onPeriod_ms = LED_ONPERIOD_CONFIG_NOCONN;
-	led_offPeriod_ms = LED_OFFPERIOD_CONFIG_NOCONN;
-}
-
-
-static void wifiManCb_numConnChange(uint8_t numConnStations, void* userVar)
-{
-	if( numConnStations > 0 )
-	{
-		led_onPeriod_ms = LED_ONPERIOD_CONFIG_CONN;
-		led_offPeriod_ms = LED_OFFPERIOD_CONFIG_CONN;
-	}
-	else
-	{
-		led_onPeriod_ms = LED_ONPERIOD_CONFIG_NOCONN;
-		led_offPeriod_ms = LED_OFFPERIOD_CONFIG_NOCONN;
-	}
-}
-
-
-static void wifiManCb_associating(const char *const ssidIn, void* userVarIn)
-{
-	led_onPeriod_ms = LED_ONPERIOD_ASSOCIATING_MS;
-	led_offPeriod_ms = LED_OFFPERIOD_ASSOCIATING_MS;
+	led_onPeriod_ms = LED_ONPERIOD_CONFIG;
+	led_offPeriod_ms = LED_OFFPERIOD_CONFIG;
 }
 
 
@@ -153,14 +132,14 @@ static void wifiManCb_associated(const char *const ssidIn, void* userVarIn)
 	led_onPeriod_ms = LED_ONPERIOD_ASSOCIATED_MS;
 	led_offPeriod_ms = LED_OFFPERIOD_ASSOCIATED_MS;
 
-	cxa_mqtt_client_network_connectToHost(&mqttC, "m11.cloudmqtt.com", 13164, "arsinio", "tmpPasswd", false);
+	cxa_mqtt_client_network_connectToHost(&mqttClient, "m11.cloudmqtt.com", 13164, "arsinio", "tmpPasswd", false);
 }
 
 
 static void mqttCb_onConnect(cxa_mqtt_client_t *const clientIn, void* userVarIn)
 {
 	uint8_t payload[] = "hello world";
-	cxa_mqtt_client_publish(&mqttC.super, CXA_MQTT_QOS_ATMOST_ONCE, false, "testTopic", payload, sizeof(payload));
+	cxa_mqtt_client_publish(&mqttClient.super, CXA_MQTT_QOS_ATMOST_ONCE, false, "testTopic", payload, sizeof(payload));
 }
 
 
